a5b94e4697e5d6752c3fa550387e7e0288ce0975
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2023-03-27 12:09:00 +0200

    update zotoer types with number sequence

--- zotero_utils.py
+++ zotero_utils.py
@@ -26,6 +26,18 @@ def update_listbibl(url, orig_file='./out/listbibl_static.xml'):
     list_bibl = orig_doc.any_xpath('.//tei:listBibl')[0]
     bibls = doc.any_xpath('.//tei:biblStruct')
     for x in tqdm(bibls, total=len(bibls)):
+        if x.attrib['type'] == "presentation":
+            x.attrib['type'] = "06_presentation"
+        if x.attrib['type'] == "encyclopediaArticle":
+            x.attrib['type'] = "05_encyclopediaArticle"
+        if x.attrib['type'] == "bookSection":
+            x.attrib['type'] = "04_bookSection"
+        if x.attrib['type'] == "book":
+            x.attrib['type'] = "01_book"
+        if x.attrib['type'] == "journalArticle":
+            x.attrib['type'] = "03_journalArticle"
+        if x.attrib['type'] == "thesis":
+            x.attrib['type'] = "02_thesis"
         zot_id = x.attrib['corresp']
         zot_xml_id = f"zotero__{zot_id.split('/')[-1]}"
         x.attrib['{http://www.w3.org/XML/1998/namespace}id'] = zot_xml_id
