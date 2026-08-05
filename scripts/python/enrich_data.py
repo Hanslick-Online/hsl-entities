1156649ae90f5d4813c4c02db1ac4dcf6f902860
fsanzl <>
2026-08-05 13:19:30 +0200

    added wikidata to works

--- scripts/python/enrich_data.py
+++ scripts/python/enrich_data.py
@@ -7,6 +7,12 @@ enrich_data(br_table_id=TABLE_ID, uri="gnd",
             field_name_update={"wikidata": "wikidata", "gnd": "gnd",
                                "oeml": "oeml", "oebl": "oebl", "pmb": "pmb"})
 
+TABLE_ID = "1499"
+enrich_data(br_table_id=TABLE_ID, uri="gnd",
+            field_name_input={"gnd": "gnd", "wikidata": "wikidata"},
+            field_name_update={"wikidata": "wikidata", "gnd": "gnd",
+                               "oeml": "oeml", "oebl": "oebl", "pmb": "pmb"})
+
 TABLE_ID = "1500"
 enrich_data(br_table_id=TABLE_ID,
             uri="geonames",
